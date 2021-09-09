61a53627f73748a2fbbf588449f57a297c1c84be
JhonEdwin_Acevedo <jhon.acevedo01@uptc.edu.co>
2021-09-09 16:45:51 -0500

    Se creo el metodo de agregar nodo al final

--- src/models/LinkedList.java
+++ src/models/LinkedList.java
@@ -2,4 +2,12 @@ package models;
 
 public abstract class LinkedList<T> {
     protected Node<T> first;
+
+    public void addElement(Node<T> node) {
+        Node<T> actual = first;
+        while (null != actual.getNext()) {
+            actual = actual.getNext();
+        }
+        actual.setNext(node);
+    }
 }
